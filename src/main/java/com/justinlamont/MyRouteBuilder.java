63c24f5ac00a4e1035748c65aa30a5cceffde984
Justin LaMont <lamontjl@gmail.com>
2021-04-15 17:49:28 -0700

    tear it down

--- src/main/java/com/justinlamont/MyRouteBuilder.java
+++ src/main/java/com/justinlamont/MyRouteBuilder.java
@@ -7,7 +7,8 @@ public class MyRouteBuilder extends RouteBuilder {
     public void configure() {
 
         System.out.println("Help!");
-
+        from("file:src/data?noop=true")
+            .to("Sup");
     }
-
+    /*.to("salesforce:createSObject")*/
 }
